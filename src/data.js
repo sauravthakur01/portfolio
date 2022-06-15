04d2b1a533d0995fae487fd3352a11bd9c8caff1
Sanjay Pal <sanjaypal001@gmail.com>
2022-06-15 17:00:04 +0530

    first

---
+++ src/data.js
@@ -0,0 +1,98 @@
+
+const projects = [
+  {
+    title: "To Do List with Database",
+    subtitle: "Node.js and Express.js and MongoDB",
+    description:
+      "Bulid using node.js , express.js ,mongodb in which we can store our items and also delete them . we can also create custom list ",
+    image:'https://i.postimg.cc/3rdZd7gz/todolist.gif',
+    link: "https://to-dolistv2.herokuapp.com",
+  },
+  {
+    title: "Simon game",
+    subtitle: "jQuery , Javascript , HTML , CSS",
+    description:
+      "Simon game is a fun game and is build using javascript css  html . Go click on this and have a try and see upto which level you reach",
+    image: "https://i.postimg.cc/Y2Rwq6zd/Simon.gif",
+    link: "https://simoongame.herokuapp.com/",
+  },
+  {
+    title: "DrumKit",
+    subtitle: "Javascript Html and CSS",
+    description:
+      "wanna try drumroll , click here . you can control this drumkit using keypad or by clicking" ,
+    image: "https://i.postimg.cc/k4c1vyjG/Drum-Kit.gif",
+    link: "https://drumskit.herokuapp.com/",
+  },
+  {
+    title: "Signup Newsletter",
+    subtitle: "Bootstrap Node.js and Express.js",
+    description:
+      "This is a simple Sign up news letter page using MailChimp ,with sucess and failure page",
+    image: "https://i.postimg.cc/c4yq1Fpy/Newsletter-Signup.gif",
+    link: "https://news1-letter.herokuapp.com/",
+  },
+  {
+    title: "Notebook",
+    subtitle: "MERN Stack",
+    description:
+      "Create delete and update your notes using this link . You need to register and login on this before using it. Your password is completely secured ",
+    image: "https://i.postimg.cc/d1ZqpLy8/Notebook-your-notes-in-cloud.gif",
+    link: "https://notebooksafe.herokuapp.com/",
+  },
+  {
+    title: "TextUtilities",
+    subtitle: "React Hooks and Bootstrap",
+    description:
+      "Text Utilitis help you to UPPERCASE , lowercase , copy to clipboard and remove extraspaces . It has dark and Light mode",
+    image: "https://i.postimg.cc/zXrgw1sd/Text-Utils-Word-counter-character-counter-lowercase-to-uppercase-uppercase-to-lowercase.gif",
+    link: "https://utilitiestexts.herokuapp.com/",
+  },
+  {
+    title: "React Firebase",
+    subtitle: "React Hooks ,Bootstrap and Firebase",
+    description:
+      "This is a responsive app and build using bootstrap , react and firebase for database and deployment ",
+    image: "https://i.postimg.cc/Y903HwrN/Tech.gif",
+    link: "https://fir-database-725cc.web.app/",
+  },
+  {
+    title: "To do list react",
+    subtitle: "React Hooks ",
+    description:
+      "This to do list app is build using react where you can add your to-do list item and remove itby simple click",
+    image: "https://i.postimg.cc/mkLqzG8y/React-App.gif",
+    link: "https://zn93kg.csb.app/",
+  },
+   {
+    title: "Keepers app",
+    subtitle: "React Hooks",
+    description:
+      "Similar to the Keep App , you can add notes and delete notes on keeper apps . This is build using react and css",
+    image: "https://i.postimg.cc/Hx20xqCx/Keeper-App.gif",
+    link: "https://k4wmmy.csb.app/",
+  },
+  {
+    title: "Link shortner",
+    subtitle: "React and Api ",
+    description:
+      "link shortner help to shorten your long url . its build using react ,api . click here and short your long urls ",
+    image: "https://shrtco.de/VHlipi",
+    link: "https://precious-truffle-d77871.netlify.app/",
+  },
+  {
+    title: "NewsPanda",
+    subtitle: "React , FetchApi and Bootstrap ",
+    description:
+      "NewsPanda is a great website where we can read our daily news . it has build in Section for entertainment sports,etc . Unfortunately this app is unavailabe on internet due to limitation of news api to use it in localhost",
+    image: "https://i.postimg.cc/KYW7rgvz/General-News-Panda.gif",
+    link: "#",
+  },
+  
+];
+
+const skills = [
+  "CSS"," HTML", "Bootstrap", "JAVASCRIPT", "MongoDB" ,"SQL" , "React.js" , "Git" , "Github " , "jQuery" , "Express.js" ,"Node.js"
+];
+
+export { projects , skills  };
